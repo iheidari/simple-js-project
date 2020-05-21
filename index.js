f8c7f07c0174feaccfc66948865c54bad6ce172a
Iman Heidari <iman.heydari@gmail.com>
2020-05-20 21:10:28 -0700

    show result in page

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function handleOnClick() {
-  console.log("clicked");
   var number1 = prompt("Enter first number", "0");
   number1 = parseInt(number1);
   var number2 = prompt("Enter second number", "0");
   number2 = parseInt(number2);
-
-  console.log("number1: ", number1);
-  console.log("number2: ", number2);
-  console.log("total:", number1 + number2);
+  var sum = number1 + number2;
+  // find the div
+  var resultSumDiv = document.getElementById("resultSum");
+  // change the content with sum of num1 and num2
+  resultSumDiv.innerText = sum;
 }
