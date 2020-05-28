c09742dadb5a6c1092ef4a05862161ead5a99e33
Iman Heidari <iman.heydari@gmail.com>
2020-05-27 21:38:31 -0700

    code flow

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
 function handleOnClick() {
-  var number1 = prompt("Enter first number", "0");
+  var number1 = prompt("Enter your score", "0");
   number1 = parseInt(number1);
-  var number2 = prompt("Enter second number", "0");
-  number2 = parseInt(number2);
-  var sum = number1 + number2;
-  // find the div
-  var resultSumDiv = document.getElementById("resultSum");
-  // change the content with sum of num1 and num2
-  resultSumDiv.innerText = sum;
+
+  var password = "";
+  do {
+    password = prompt("Enter the password");
+  } while (password !== "coding");
 }
+
+const whileExample = function (topNumber) {
+  var a = 0;
+  while (a <= topNumber) {
+    if (a % 2 !== 0) {
+      console.log(a);
+    }
+    a = a + 1;
+  }
+};
+const checkScore = function (score) {
+  if (score > 80) {
+    alert("A");
+  } else if (score > 60) {
+    alert("B");
+  } else if (score > 40) {
+    alert("C");
+  } else {
+    alert("D");
+  }
+};
+
+const add = function (a, b) {
+  if (Number.isNaN(a)) {
+    return 0;
+  }
+  if (Number.isNaN(b)) {
+    return 0;
+  }
+
+  a = Math.abs(a);
+  b = Math.abs(b);
+
+  return a + b;
+};
